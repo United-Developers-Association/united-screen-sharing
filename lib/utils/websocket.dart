800698e7bfa7c011d833217a0b700c6205c6f7d0
kaixo1 <161948796+kaixo1@users.noreply.github.com>
2024-03-15 17:07:18 +0100

    fix: syntax errors in `utils/websocket.dart`
    Fixed miscelaneous syntax errors in `utils/websocket.dart`

--- lib/utils/websocket.dart
+++ lib/utils/websocket.dart
@@ -1,20 +1,35 @@
 import 'dart:io' if (dart.library.js_util) 'dart:js';
+import 'dart:async';
 
-class SimpleWebSocket extends WebSocket {
+import 'package:meta/meta.dart';
+
+class WebSocketServer {
     String address;
     int port;
 
-    SimpleWebSocket(address, port);
+    WebSocketServer(this.address, this.port);
 
     HttpServer? _server;
+    Stream<WebSocket>? _sockets;
 
     Future<void> initConnection() async {
         _server = await HttpServer.bind(address, port);
-        _server.transform(WebSocketTransformer()).listen(listen);
+        _sockets = _server!.transform(WebSocketTransformer());
+        _sockets!.listen(onConnectionEstablished, onDone: () => dispose());
     }
 
-    Function()? onConnectionEstablished;
-    Function()? onConnectionListening;
-    Function()? onConnectionFinished;
-    Function()? onConnectionData;
+    /// is called when the http server is about to be closed
+    @mustCallSuper
+    void dispose() {
+        
+    }
+
+    /// is called when a websocket connection is made with a pair. can be called many different times
+    ///
+    /// in order to use, listen for websocket messages using socket.listen() or async*, or send messages using .add and .addStream
+    FutureOr<void> Function(WebSocket socket)? onConnectionEstablished;
+    /// is called when the http server is established and listening for connections
+    FutureOr<void> Function()? onConnectionListening;
+    /// is called when a websocket connection is closed (can be called many times)
+    FutureOr<void> Function(int? closeCode, String? closeReason)? onConnectionFinished;
 }
